108c624f9381bcbc23bd843751b2801d395b853b
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-05-01 09:12:22 -0700

    :lollipop: Created repository

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿Style UI for ArcGIS feature service
+===================================
+
+This project is for designing a UI that will allow the user to change the styling of feature layers in a map.
---
+++ index.js
@@ -0,0 +1,36 @@
+﻿/*global require*/
+require([
+	"esri/map",
+	"esri/renderers/smartMapping",
+	"esri/layers/FeatureLayer",
+	"esri/renderers/SimpleRenderer",
+	"esri/symbols/SimpleLineSymbol"
+], function (Map, smartMapping, FeatureLayer, SimpleRenderer, SimpleLineSymbol) {
+	var map, layer;
+
+	map = new Map("map", {
+		basemap: "hybrid",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 8,
+		showAttribution: true
+	});
+
+	map.on("load", function () {
+		layer = new FeatureLayer("http://services.arcgis.com/IYrj3otxNjPsrTRD/ArcGIS/rest/services/WSDOT%20-%20Roadway%20Characteristics/FeatureServer/1");
+		map.addLayer(layer);
+		document.getElementById("changeStyleButton").disabled = false;
+	});
+
+	var form = document.forms.styleForm;
+
+
+	form.onsubmit = function () {
+		var symbol = new SimpleLineSymbol();
+		symbol.setColor(this.color.value);
+		symbol.setWidth(this.width.value);
+		var renderer = new SimpleRenderer(symbol);
+		layer.setRenderer(renderer);
+		layer.refresh();
+		return false;
+	};
+});

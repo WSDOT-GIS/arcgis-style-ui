d01bc4ae5b51c1e9cd36f9b03a2973032fbb279c
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2015-05-01 17:01:52 -0700

    Added layer list

--- index.js
+++ index.js
@@ -16,8 +16,40 @@ require([
 		showAttribution: true
 	});
 
+	var toggleLayer = function () {
+		var id = this.value;
+		var layer = map.getLayer(id);
+		if (!layer) {
+			layer = map.getGraphicsLayer(id);
+		}
+		if (layer) {
+			if (this.checked) {
+				layer.show();
+			} else {
+				layer.hide();
+			}
+		}
+	};
+
+	map.on("layer-add", function (evt) {
+		var layer = evt.layer;
+		var layerList = document.getElementById("layerList");
+		var li = document.createElement("li");
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.value = layer.id;
+		checkbox.checked = layer.visible;
+		checkbox.addEventListener("click", toggleLayer);
+		li.appendChild(checkbox);
+		li.appendChild(document.createTextNode(layer.title || layer.id));
+		li.dataset.layerId = layer.id;
+		layerList.insertBefore(li, layerList.firstChild);
+	});
+
 	map.on("load", function () {
-		layer = new FeatureLayer("http://services.arcgis.com/IYrj3otxNjPsrTRD/ArcGIS/rest/services/WSDOT%20-%20Roadway%20Characteristics/FeatureServer/1");
+		layer = new FeatureLayer("http://services.arcgis.com/IYrj3otxNjPsrTRD/ArcGIS/rest/services/WSDOT%20-%20Roadway%20Characteristics/FeatureServer/1", {
+			id: "Speed_Limits"
+		});
 		map.addLayer(layer);
 
 		var setLayerRenderer = function (evt) {
